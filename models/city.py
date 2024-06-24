638fe00889cb7bfc6ad8a8d52c382dd8c345c042
smoxhakim <joulal1994@gmail.com>
2024-06-24 23:42:32 +0100

    bug_fix

--- models/city.py
+++ models/city.py
@@ -3,14 +3,17 @@
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String, ForeignKey
 from sqlalchemy.orm import relationship
-
+from os import getenv
 
 class City(BaseModel, Base):
     """ The city class, contains state ID and name """
     __tablename__ = "cities"
     
-    name = Column(String(128), nullable=False)
-    state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
-    
-    places = relationship("Place", backref="city", cascade="all, delete-orphan")
-
+    if getenv("HBNB_TYPE_STORAGE") == 'db':
+        state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
+        name = Column(String(128), nullable=False)
+        places = relationship("Place", backref="city", cascade="all, delete-orphan")
+        
+    else:
+        state_id = ""
+        name = "" 
