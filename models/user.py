d954c6e8264797bd0f0cc3d3f7fb34a301e0275e
smoxhakim <joulal1994@gmail.com>
2024-06-25 00:30:57 +0100

    bug_fix4

--- models/user.py
+++ models/user.py
@@ -8,7 +8,6 @@ from os import getenv
 
 class User(BaseModel, Base):
     """This class defines a user by various attributes"""
-    __tablename__ = 'users'
     
     if getenv("HBNB_TYPE_STORAGE") == 'db':
         __tablename__ = 'users'
