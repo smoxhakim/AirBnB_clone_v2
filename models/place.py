d613bab4103c65a753105746591bde7886d58c46
smoxhakim <joulal1994@gmail.com>
2024-06-24 20:49:45 +0100

    user

--- models/place.py
+++ models/place.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
 """ Place Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
 
 
 class Place(BaseModel):
     """ A place to stay """
+    
+    
     city_id = ""
     user_id = ""
     name = ""
