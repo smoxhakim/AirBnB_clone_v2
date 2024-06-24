e42923ab6a4528809606756ad1f02c098a396afd
smoxhakim <joulal1994@gmail.com>
2024-06-24 23:52:32 +0100

    bug_fix2

--- models/review.py
+++ models/review.py
@@ -1,10 +1,21 @@
 #!/usr/bin/python3
 """ Review module for the HBNB project """
 from models.base_model import BaseModel
-
+from sqlalchemy import Column, String, ForeignKey
+from sqlalchemy.orm import relationship
+from os import getenv
 
 class Review(BaseModel):
     """ Review classto store review information """
-    place_id = ""
-    user_id = ""
-    text = ""
+    
+    __tablename__ = 'reviews'
+    
+    if getenv("HBNB_TYPE_STORAGE") == 'db':
+        text = Column(String(1024), nullabal=False)
+        place_id = Column(String(60), ForeignKey(place.id), nullabal=False)
+        user_id = Column(String(60), ForeignKey(users.id), nullabal=False)
+    
+    else:
+        place_id = ""
+        user_id = ""
+        text = ""
--- models/user.py
+++ models/user.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 """This module defines a class User"""
+import models
 from models.base_model import BaseModel, Base
 from sqlalchemy import String, Column
 from sqlalchemy.orm import relationship
