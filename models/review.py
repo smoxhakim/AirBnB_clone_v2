33b611c8d3fd6ddcb6158a7e09b05554833b85c4
smoxhakim <joulal1994@gmail.com>
2024-06-25 00:28:10 +0100

    bug_fix4

--- models/review.py
+++ models/review.py
@@ -7,10 +7,9 @@ from os import getenv
 
 class Review(BaseModel, Base):
     """ Review classto store review information """
-    
-    __tablename__ = 'reviews'
-    
+        
     if getenv("HBNB_TYPE_STORAGE") == 'db':
+        __tablename__ = 'reviews'
         text = Column(String(1024), nullabal=False)
         place_id = Column(String(60), ForeignKey(place.id), nullabal=False)
         user_id = Column(String(60), ForeignKey(users.id), nullabal=False)
--- models/user.py
+++ models/user.py
@@ -11,6 +11,7 @@ class User(BaseModel, Base):
     __tablename__ = 'users'
     
     if getenv("HBNB_TYPE_STORAGE") == 'db':
+        __tablename__ = 'users'
         email = Column(String(128), nullable=False)
         password = Column(String(128), nullable=False)
         first_name = Column(String(128))
