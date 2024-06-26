b7f8b318391b5993f5b5bdeefde11f0fbe68d330
smoxhakim <joulal1994@gmail.com>
2024-06-26 18:51:15 +0100

    10.

--- models/place.py
+++ models/place.py
@@ -6,9 +6,9 @@ from os import getenv
 from sqlalchemy.orm import relationship
 
 
-place_amenity = Table('place_amenity',Base.metadata,
+place_amenity = Table('place_amenity', Base.metadata,
     Column('place_id', String(60), ForeignKey('places.id'),
-        primary_key=True,nullable=False),
+        primary_key=True, nullable=False),
     Column('amenity_id', String(60), ForeignKey('amenities.id')),
         primary_key=True, nullable=False)
 
