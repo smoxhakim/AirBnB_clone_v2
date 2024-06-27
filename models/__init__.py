7d5a391fddf744c521d1cc8de11f3b5c9afd9cc6
Abdeljalil Ouafi <abdeljalilouafi44@gmail.com>
2024-06-27 01:15:14 +0100

    states.id

--- models/__init__.py
+++ models/__init__.py
@@ -2,11 +2,16 @@
 """This module instantiates an object of class FileStorage"""
 from os import getenv
 
+# print(getenv('HBNB_TYPE_STORAGE'))
+# print(getenv('HBNB_MYSQL_USER'))
+
 if getenv('HBNB_TYPE_STORAGE') == 'db':
     from models.engine.db_storage import DBStorage
+    # print("Using DABABASE...")
     storage = DBStorage()
     storage.reload()
 else:
     from models.engine.file_storage import FileStorage
+    # print("Using FILESTORAGE...")
     storage = FileStorage()
     storage.reload()
--- models/city.py
+++ models/city.py
@@ -12,9 +12,8 @@ class City(BaseModel, Base):
     __tablename__ = "cities"
 
     if getenv("HBNB_TYPE_STORAGE") == 'db':
-        __tablename__ = "cities"
-        state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
         name = Column(String(128), nullable=False)
+        state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
         places = relationship(
             "Place", backref="cities", cascade="all, delete-orphan")
 
