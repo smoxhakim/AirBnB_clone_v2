49e8051a3496895ab9ae99160a97fe3ee3fb5625
smoxhakim <joulal1994@gmail.com>
2024-07-11 07:36:10 +0100

    1-pack_web_static.py

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,1 +1,16 @@
 #!/usr/bin/python3
+""" Fabric script """
+from fabric.api import local
+from datetime import datetime
+
+
+def do_pack():
+    """ generates a .tgz archive from the contents of the web_static folder """
+    try:
+        local("mkdir -p versions")
+        date_time = datetime.now().strftime("%Y%m%d%H%M%S")
+        path = "versions/web_static_{}.tgz".format(date_time)
+        local("tar -cvzf {} web_static".format(path))
+        return path
+    except Exception:
+        return None
