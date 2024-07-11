188125173eca879aa3dce99a35b79c08469e1c0c
smoxhakim <joulal1994@gmail.com>
2024-07-11 17:30:02 +0100

    2-do_deploy_web_static.py

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -51,7 +51,7 @@ def do_pack():
 """
 
 
-"""def do_deploy(archive_path):
+def do_deploy(archive_path):
     #distributes an archive to the web servers
     if exists(archive_path) is False:
         return False
@@ -67,37 +67,6 @@ def do_pack():
         run('rm -rf {}{}/web_static'.format(path, no_ext))
         run('rm -rf /data/web_static/current')
         run('ln -s {}{}/ /data/web_static/current'.format(path, no_ext))
-        return True
-    except Exception:
-        return False"""
-
-  
-def do_deploy(archive_path):
-    """ deploy an archive to your web servers """
-    if not os.path.exists(archive_path):
-        return False
-    try:
-        releases_path = "/data/web_static/releases/"
-        file_name = os.path.basename(archive_path)
-        file_name_no_ext = file_name.split(".")[0]
-        current = "/data/web_static/current"
-
-        put(archive_path, '/tmp/')
-        # check if the path dosen't exist, it must be created
-        if not exists(releases_path):
-            sudo(f"mkdir -p {releases_path}")
-            sudo(f"mkdir -p /data/web_static/shared/")
-        sudo(f"tar -xzf /tmp/{file_name} -C {releases_path} && sudo mv \
-            {releases_path}web_static {releases_path}{file_name_no_ext}")
-        run(f"rm '/tmp/{file_name}'")
-
-        # checks if "current" exists on the server before attempting to delete
-        if exists(current):
-            sudo(f"rm {current}")
-        else:
-            pass
-        sudo(f"ln -s {releases_path}{file_name_no_ext} {current}")
-        print("New version deployed!")
         return True
     except Exception:
         return False
