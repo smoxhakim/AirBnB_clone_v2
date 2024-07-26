4193ba2b193be2eab91d63757233f3db51246dbb
smoxhakim <joulal1994@gmail.com>
2024-07-26 07:16:11 +0100

    3-python_route.py

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -1,29 +1,31 @@
 #!/usr/bin/python3
+"""Write a script that starts a Flask web application"""
 from flask import Flask
 
 HBNB_app = Flask(__name__)
 
-@HBNB_app.route("/", strict_slashes=False )
+
+@HBNB_app.route("/", strict_slashes=False)
 def home():
     return "Hello HBNB!"
 
-@HBNB_app.route("/hbnb", strict_slashes=False )
+
+@HBNB_app.route("/hbnb", strict_slashes=False)
 def home_HBNB():
     return "HBNB"
 
-@HBNB_app.route("/c/<text>", strict_slashes=False )
+
+@HBNB_app.route("/c/<text>", strict_slashes=False)
 def home_args(text):
     text = text.replace('_', ' ')
-                
     return "<H1>C {}".format(text)
 
-@HBNB_app.route("/python/", defaults={'text': 'is cool'}, strict_slashes=False )
-@HBNB_app.route("/python/<text>", strict_slashes=False )
+
+@HBNB_app.route("/python/", defaults={'text': 'is cool'}, strict_slashes=False)
+@HBNB_app.route("/python/<text>", strict_slashes=False)
 def home_python(text):
     text = text.replace('_', ' ')
-                
     return "<H1>C {}".format(text)
-
 
 
 if __name__ == "__main__":
