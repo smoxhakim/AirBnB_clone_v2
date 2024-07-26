13970cbdb2ee9ec1dc175a58d9600e81e38a18d4
smoxhakim <joulal1994@gmail.com>
2024-07-26 07:18:58 +0100

    4-number_route.py

--- web_flask/4-number_route.py
+++ web_flask/4-number_route.py
@@ -1,31 +1,34 @@
 #!/usr/bin/python3
-
+"""Write a script that starts a Flask web application"""
 from flask import Flask, abort
 
 HBNB_app = Flask(__name__)
 
-@HBNB_app.route("/", strict_slashes=False )
+
+@HBNB_app.route("/", strict_slashes=False)
 def home():
     return "Hello HBNB!"
 
-@HBNB_app.route("/hbnb", strict_slashes=False )
+
+@HBNB_app.route("/hbnb", strict_slashes=False)
 def home_HBNB():
     return "HBNB"
 
-@HBNB_app.route("/c/<text>", strict_slashes=False )
+
+@HBNB_app.route("/c/<text>", strict_slashes=False)
 def home_args(text):
     text = text.replace('_', ' ')
-                
     return "<H1>C {}".format(text)
 
-@HBNB_app.route("/python/", defaults={'text': 'is cool'}, strict_slashes=False )
-@HBNB_app.route("/python/<text>", strict_slashes=False )
+
+@HBNB_app.route("/python/", defaults={'text': 'is cool'}, strict_slashes=False)
+@HBNB_app.route("/python/<text>", strict_slashes=False)
 def home_python(text):
     text = text.replace('_', ' ')
-                
     return "<H1>C {}".format(text)
 
-@HBNB_app.route("/number/<int:n>", strict_slashes=False )
+
+@HBNB_app.route("/number/<int:n>", strict_slashes=False)
 def home_num(n):
     if (isinstance(n, int)):
         return "<H1>{}</H1>".format(n)
