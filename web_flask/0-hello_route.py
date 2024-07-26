18751ec1dee97ad6212407ba51eba8e52ed54897
smoxhakim <joulal1994@gmail.com>
2024-07-26 07:06:59 +0100

    0-hello_route.py

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,10 +1,13 @@
 #!/usr/bin/python3
+"""Write a script that starts a Flask web application"""
 from flask import Flask
 
 HBNB_app = Flask(__name__)
 
+
 @HBNB_app.route("/",  strict_slashes=False)
 def home():
+    """home"""
     return "<H1>Hello HBNB!</H1>"
 
 
