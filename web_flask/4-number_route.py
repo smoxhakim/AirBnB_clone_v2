e66e95250c2fe1eab58348824bccd98e755dc80d
smoxhakim <joulal1994@gmail.com>
2024-07-26 07:31:33 +0100

    4-number_route.py

--- web_flask/4-number_route.py
+++ web_flask/4-number_route.py
@@ -31,7 +31,7 @@ def home_python(text):
 @HBNB_app.route("/number/<int:n>", strict_slashes=False)
 def home_num(n):
     if (isinstance(n, int)):
-        return "{}".format(n)
+        return "{} is an integer".format(n)
     else:
         abort(404)
 
