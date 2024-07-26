ad1b30160ad59a84ea8e93cb65341e644b365156
smoxhakim <joulal1994@gmail.com>
2024-07-26 21:33:51 +0100

    7-states_list.py

---
+++ web_flask/7-states_list.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""flask web application that lists states dynamiclly"""
+from flask import Flask, render_template
+
+from models import storage
+from models.state import State
+
+db_app = Flask(__name__)
+
+
+@db_app.route("/states_list")
+def states():
+    """ states list"""
+    states_li = storage.all(State)
+    if states_li:
+        render_template("7-states_list.html", states_li=states_li)
+    else:
+        return "Storage wasn't imported"
+
+@db_app.teardown_appcontext
+def close_dataB():
+    """ states list"""
+    storage.close()
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000, debug=True)
