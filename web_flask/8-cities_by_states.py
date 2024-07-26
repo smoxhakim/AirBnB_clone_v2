daf2a799fb298da34535d680936eb35385c6445e
smoxhakim <joulal1994@gmail.com>
2024-07-27 00:17:15 +0100

    8-cities_by_states.py

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -10,14 +10,14 @@ app = Flask(__name__)
 
 
 @app.route('/cities_by_states', strict_slashes=False)
-def cities_by_states():
+def city_by_state():
     """display the states and cities listed in alphabetical order"""
     states = storage.all("State").values()
     return render_template('8-cities_by_states.html', states=states)
 
 
 @app.teardown_appcontext
-def teardown_db(exception):
+def close_dataB(exception):
     """closes the storage on teardown"""
     storage.close()
 
